dfcb2179a00cdf8e2fd44a4bdfaea07bc8a0ca81
dlegarda <legarda8027@gmail.com>
2024-10-21 06:58:29 -0500

    cambio de conexion

--- src/Basedatos/ConexionMongoDB.js
+++ src/Basedatos/ConexionMongoDB.js
@@ -4,9 +4,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const conectarDB = async () => {
-  console.log(process.env.MONGODB_URI);
+  //conexion a mongoDB
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect("mongodb+srv://legarda8027:HOUMJOPOLhxqWfEV@cluster0.s4wly.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
     console.log('Conexión a MongoDB exitosa');
   } catch (error) {
     console.error('Error al conectar a MongoDB:', error);
