a4fbad249fa76e03a8744207859f89fafd7b0fbd
dlegarda <legarda8027@gmail.com>
2024-10-21 06:54:38 -0500

    envio de url

--- src/Basedatos/ConexionMongoDB.js
+++ src/Basedatos/ConexionMongoDB.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const conectarDB = async () => {
+  console.log(process.env.MONGODB_URI);
   try {
     await mongoose.connect(process.env.MONGODB_URI);
     console.log('Conexión a MongoDB exitosa');
